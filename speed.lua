a3487c2361b5afd975aaa9a8e68a7069a70b27bb
L5 <100592537+L5-cg@users.noreply.github.com>
2022-10-13 01:38:19 -0400

    Create speed.lua

---
+++ speed.lua
@@ -0,0 +1,18 @@
+ while true do
+	for _, child in ipairs(workspace.Cars:GetChildren()) do
+		if child:IsA("Part")then
+			child.Driver.MaxSpeed = speed
+		else
+			child.Driver.MaxSpeed = speed
+		end
+	end
+	wait(0.1)
+	for _, child in ipairs(workspace.Cars:GetChildren()) do
+		if child:IsA("Part")then
+			child.Driver.MaxSpeed = speed
+		else
+			child.Driver.MaxSpeed = speed
+		end
+	end
+	wait(0.1)
+end 
